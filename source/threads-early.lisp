cca2c55133fcf439f2525cf659ee0bc95d1be792
Alexander Gavrilov <angavrilov@gmail.com>
2010-02-08 19:51:07 +0000

    A couple of portability tweaks.

--- source/threads-early.lisp
+++ source/threads-early.lisp
@@ -16,7 +16,7 @@
   "Increments a special place holding a machine word sized integer in a way that paralell CPU's don't step on each other's toes when incrementing/decrementing it. These operation don't expand to bignums, they just overflow/underflow when the high/low bound of the word sized integer is crossed."
   #*((:sbcl (sb-ext:atomic-incf (ac/counter atomic-counter) delta))
      (t #.(warn "HU.DWIM.UTIL:ATOMIC-COUNTER is not thread-safe on your platform!")
-        (incf (ac/counter atomic-counter delta)))))
+        (incf (ac/counter atomic-counter) delta))))
 
 (def (function ioe) atomic-counter/decrement (atomic-counter &optional (delta 1))
   "A convenience version of ATOMIC-COUNTER/INCREMENT, mostly to keep the user code readable."
