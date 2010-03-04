8cb5d14cc5bcded74770b347b5b59e755f3b2e17
attila.lendvai <attila.lendvai@gmail.com>
2010-03-04 18:48:57 +0000

    with-deadline compiles on platforms it doesn't support and errors at runtime unless :optional is #t

--- source/threads.lisp
+++ source/threads.lisp
@@ -18,11 +18,13 @@
                                       (setf (find-thread-id thread)
                                             (atomic-counter/increment *thread-id-counter*)))))
 
-(def (macro e) with-deadline ((timeout-in-seconds) &body body)
+(def (macro e) with-deadline ((timeout-in-seconds &key (optional #f)) &body body)
+  (declare (ignorable optional))
   "As usual, TIMEOUT is in the SI unit of the scale, which is seconds. Will signal a DEADLINE-TIMEOUT when the deadline is passed."
   #*((:sbcl `(sb-sys:with-deadline (:seconds ,timeout-in-seconds)
                ,@body))
-     (t #.(not-yet-implemented/crucial-api 'with-deadline))))
+     (t (unless optional
+          #.(not-yet-implemented/crucial-api 'with-deadline)))))
 
 #*((:sbcl
      (import 'sb-sys:deadline-timeout :hu.dwim.util)
