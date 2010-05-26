65affd658f36dcee34b7f8c9bfcfc756a88a6468
attila.lendvai <attila.lendvai@gmail.com>
2010-05-26 11:37:19 +0000

    simplify sum and product

--- source/number.lisp
+++ source/number.lisp
@@ -7,29 +7,29 @@
 (in-package :hu.dwim.util)
 
 (def (function e) sum (&rest args)
-  (funcall #'sum* args))
+  (sum* args))
 
-(def (function e) sum* (list &key (key #'identity) ignore-non-numbers)
-  (reduce (lambda (a b)
-            (if (numberp b)
-                (+ a b)
-                (if ignore-non-numbers
-                    a
-                    (error "~S is not a number" b))))
+(def (function e) sum* (list &key key ignore-non-numbers)
+  (reduce (if ignore-non-numbers
+              (lambda (a b)
+                (if (numberp b)
+                    (+ a b)
+                    a))
+              #'+)
           list
           :key key
           :initial-value 0))
 
 (def (function e) product (&rest args)
-  (funcall #'product* args))
+  (product* args))
 
-(def (function e) product* (list &key (key #'identity) ignore-non-numbers)
-  (reduce (lambda (a b)
-            (if (numberp b)
-                (* a b)
-                (if ignore-non-numbers
-                    a
-                    (error "~S is not a number" b))))
+(def (function e) product* (list &key key ignore-non-numbers)
+  (reduce (if ignore-non-numbers
+              (lambda (a b)
+                (if (numberp b)
+                    (* a b)
+                    a))
+              #'*)
           list
           :key key
           :initial-value 1))
