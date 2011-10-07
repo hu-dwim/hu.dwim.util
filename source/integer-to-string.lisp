469e3c6be63da64425730a637be9f444ef5b57d4
attila.lendvai <attila.lendvai@gmail.com>
2011-10-07 17:52:24 +0000

    properly support non-base-10 usage of integer-to-string

--- source/integer-to-string.lisp
+++ source/integer-to-string.lisp
@@ -11,18 +11,20 @@
 
 (def constant +integer-to-string-cache-size+ 128)
 
-(def (constant :test 'equalp) +cached-integer-names+
+(def (constant :test 'equalp) +cached-integer-names/base-10/unindented+
     (coerce (iter (for idx :from 0 :below +integer-to-string-cache-size+)
                   (collect (coerce (princ-to-string idx) 'simple-base-string)))
             `(simple-array string (,+integer-to-string-cache-size+))))
 
-(def (function io) %integer-to-string (integer &key minimum-column-count (maximum-digit-count most-positive-fixnum) (divisor 10))
+(def (function io) %integer-to-string (integer minimum-column-count maximum-digit-count base)
   (declare (type integer integer)
            (type (or null fixnum) minimum-column-count)
            (type fixnum maximum-digit-count))
   (if (and (< -1 integer +integer-to-string-cache-size+)
-           (null minimum-column-count))
-      (aref +cached-integer-names+ integer)
+           (null minimum-column-count)
+           (= maximum-digit-count most-positive-fixnum)
+           (= base 10))
+      (aref +cached-integer-names/base-10/unindented+ integer)
       (bind ((remainder integer)
              (digit 0)
              (digits-count 0)
@@ -36,7 +38,7 @@
                         (setf (aref result result-index) ,char)
                         (incf result-index))))
           (iter (repeat maximum-digit-count)
-                (setf (values remainder digit) (truncate remainder divisor))
+                (setf (values remainder digit) (truncate remainder base))
                 (push digit digits)
                 (incf digits-count)
                 (until (zerop remainder)))
@@ -46,21 +48,18 @@
                 (iter (repeat padding-length)
                       (emit #\0)))))
           (dolist (digit digits)
-            (emit (code-char (+ #x30 digit)))))
+            (emit (code-char (+ (if (<= digit 9)
+                                    #.(char-code #\0)
+                                    #.(- (char-code #\a) 10))
+                                digit)))))
         (bind ((real-result (make-array result-index :element-type 'base-char)))
           (replace real-result result :end1 result-index)
           real-result))))
 
-(def (function eo :inline :possible) integer-to-string (integer &key minimum-column-count (maximum-digit-count most-positive-fixnum) (divisor 10))
+(def (function eo :inline :possible) integer-to-string (integer &key minimum-column-count (maximum-digit-count most-positive-fixnum) (base 10))
   (check-type integer integer)
   (check-type minimum-column-count (or null fixnum))
   (check-type maximum-digit-count fixnum)
   (etypecase integer
-    (fixnum (%integer-to-string integer
-                                :minimum-column-count minimum-column-count
-                                :maximum-digit-count maximum-digit-count
-                                :divisor divisor))
-    (integer (%integer-to-string integer
-                                 :minimum-column-count minimum-column-count
-                                 :maximum-digit-count maximum-digit-count
-                                 :divisor divisor))))
+    (fixnum (%integer-to-string integer  minimum-column-count  maximum-digit-count base))
+    (integer (%integer-to-string integer minimum-column-count maximum-digit-count base))))
